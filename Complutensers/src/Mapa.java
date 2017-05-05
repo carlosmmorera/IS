486d632ad4cbf49e6b31458e25fbf5680bd07f57
Carlos Moreno <carmor06@ucm.es>
2017-05-05 17:27:19 +0200

    Version 1.0

---
+++ Complutensers/src/Mapa.java
@@ -0,0 +1,24 @@
+
+public class Mapa {
+	/**
+	 * @param direccion
+	 * @return coordinates associated with the address.
+	 */
+	public Coordenada obtenerCoordenada(String direccion){
+		return null;
+	}
+	/**
+	 * @param coordenada
+	 * @return address associated wit the coordinates.
+	 */
+	public String obtenerDireccion(Coordenada coordenada){
+		return null;
+	}
+	/**
+	 * Shows the place in the map.
+	 * @param lugar
+	 */
+	public void mostrarEnMapa(Lugar lugar){
+		
+	}
+}
