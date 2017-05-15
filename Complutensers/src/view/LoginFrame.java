2dcb1f07f5dc1bccaac04cef2407ee72455ca5d6
rinicro <ruben.ruper@gmail.com>
2017-05-15 12:15:49 +0200

    Un LoginFrame to guapo

--- Complutensers/src/view/LoginFrame.java
+++ Complutensers/src/view/LoginFrame.java
@@ -1,17 +1,22 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class LoginFrame extends JFrame{
 	
 	JTextField user;
-	JTextField password;
-	JButton log;
+	JPasswordField password;
+	JButton logButton;
+	JButton notlogButton;
 	
 	public LoginFrame (){
 		super("Login");		
@@ -19,28 +24,40 @@ public class LoginFrame extends JFrame{
 	}
 	
 	private void initGUI(){		
-		//JPanel mainpanel = createJpanel();
-		JPanel panel=new JPanel();
-		user = new JTextField();
-		user.setSize(120, 30);
-		password = new JTextField();
-		password.setSize(120, 30);
-		panel.add(user);
-		panel.add(password);
+		JPanel panel = createJpanel();		
 		this.add(panel);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setPreferredSize(new Dimension(600, 600));
 		this.pack();
 		this.setVisible(true);
 	}
 	private JPanel createJpanel(){
 		JPanel panel=new JPanel();
-		user = new JTextField();
-		user.setSize(120, 30);
-		password = new JTextField();
-		password.setSize(120, 30);
-		panel.add(user);
-		panel.add(password);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JPanel userp = new JPanel();
+		userp.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
+		user = new JTextField(20);			
+		JLabel a = new JLabel("Usuario:");
+		userp.add(a);
+		userp.add(user);
+		
+		JPanel pass = new JPanel();
+		pass.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
+		password = new JPasswordField(20);	
+		JLabel b = new JLabel("Contraseña:");
+		pass.add(b);
+		pass.add(password);
+		
+		JPanel botones = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 20));
+		botones.setSize(new Dimension(500,100));		
+		logButton = new JButton("Log in");
+		notlogButton = new JButton("Entrar sin log");
+		botones.add(logButton);
+		botones.add(notlogButton);
+		
+		panel.add(userp);
+		panel.add(pass);
+		panel.add(botones);
 		return panel;
 	}
 
