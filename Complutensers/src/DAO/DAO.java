a60bab3bfbf5e0826590811730155089e45f91c0
Carlos Moreno <carmor06@ucm.es>
2017-05-15 12:26:26 +0200

    DAO

---
+++ Complutensers/src/DAO/DAO.java
@@ -0,0 +1,8 @@
+package DAO;
+
+public class DAO {
+	final static String NAME_STUDENT = "loginStudent.txt";
+	final static String NAME_AE = "loginAE.txt";
+	final static String NAME_ACTIVITIES = "activities.txt";
+	final static String NAME_INITIATIVES = "initiatives.txt";
+}
